220d445249b348c682f9f461fe884e90a7c5a08a
Abdul Al Roman <mdroman45678910@gmail.com>
2025-12-25 14:26:24 +0600

    feat:(dashboard):make the basics structre of dashboard

---
+++ app/(withDashboardLayout)/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export default function DashbordLayout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      DashbordLayout
+      {children}
+    </div>
+  );
+}
